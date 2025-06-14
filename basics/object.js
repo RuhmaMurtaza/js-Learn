b02dc6bd7525bab3c3f89123125adb8e51ad5618
RuhmaMurtaza <ruhmaamurtaza@gmail.com>
2025-06-14 20:15:36 +0000

    objects

---
+++ basics/object.js
@@ -0,0 +1,14 @@
+// objects are used in js to store values in form of keys and values
+
+let ruhmasData={
+    Name:"Ruhma",
+    Department:"Information Technology",
+    Semester:"8th",
+    Email:"ruhmaamurtaza@gmail.com"
+}
+
+//  access keys and valus of object like this
+
+console.log(ruhmasData.Department)
+// the other method is 
+console.log(ruhmasData["Department"])
