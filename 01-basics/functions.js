c60cad00412d2e50809109fd62dea69be1d36e96
RuhmaMurtaza <ruhmaamurtaza@gmail.com>
2025-07-07 13:44:29 +0000

    update

--- 01-basics/functions.js
+++ 01-basics/functions.js
@@ -3,16 +3,39 @@
 
 function loginUser(username){
     if(username === undefined){
-        console.log('Please Enter userName') 
+       // console.log('Please Enter userName') 
         return
     }
     return `${username} just logged in `
 }
 
-console.log(loginUser('Ruhii'))
+//console.log(loginUser('Ruhii'))
 
 
 // the fuction can contain parameters 
 // 1. we need to do console log to call function
 // 2. if duringsome conditions in function we want half func to execute use returen statment ater that no 
 // other statement will execute
+
+
+// ########################### object as arguement in func ############################
+
+let userData = {
+    Name : 'Ruhma' ,
+    Price : 100
+}
+
+function user(anyobj){
+    console.log(`The user ${anyobj.Name} has price ${anyobj.Price}`)
+}
+
+(user(userData))
+
+
+//###################### if we want to pass multiple value we can do this ...(rest method) ###########
+
+function multValues(... num1){
+    return num1
+}
+
+console.log(multValues(200,300,1000))
